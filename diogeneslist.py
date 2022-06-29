abd766f73ed1f883ea73e57929965a2a3cd30135
ZapperDJ <zapperdj@gmail.com>
2022-06-29 16:18:48 +0200

    Fix inconsistent use of spaces and tabs

--- diogeneslist.py
+++ diogeneslist.py
@@ -47,20 +47,20 @@ def generateDirArray(dirToScan):
         currentDirArray.append(currentDirFixed+'*0*'+currentDirModifiedTime)   # append directory info to currentDirArray 
         totalSize = 0
         for file in files:
-			numFiles = numFiles + 1
-			fileSize = getsize(currentDir+'/'+file)
-			totalSize = totalSize + fileSize
-			grandTotalSize = grandTotalSize + fileSize
-			fileModifiedTime = datetime.datetime.fromtimestamp(os.path.getmtime(currentDir+'/'+file))
-			fileModifiedTime = fileModifiedTime.strftime("%d/%m/%Y %H:%M:%S")
-			currentDirArray.append(file+'*'+str(fileSize)+'*'+fileModifiedTime)   # append file info to currentDirArray
+            numFiles = numFiles + 1
+            fileSize = getsize(currentDir+'/'+file)
+            totalSize = totalSize + fileSize
+            grandTotalSize = grandTotalSize + fileSize
+            fileModifiedTime = datetime.datetime.fromtimestamp(os.path.getmtime(currentDir+'/'+file))
+            fileModifiedTime = fileModifiedTime.strftime("%d/%m/%Y %H:%M:%S")
+            currentDirArray.append(file+'*'+str(fileSize)+'*'+fileModifiedTime)   # append file info to currentDirArray
         currentDirArray.append(totalSize)   # append total file size to currentDirArray
         # create the list of directory IDs correspondent to the subdirs present on the current directory
         # this acts as a list of links to the subdirectories on the javascript code
         dirLinks = ''
         for dir in dirs:
-			numDirs = numDirs + 1
-			dirLinks = dirLinks + str(dirIDsDictionary[currentDir+'/'+dir]) + '*'
+            numDirs = numDirs + 1
+            dirLinks = dirLinks + str(dirIDsDictionary[currentDir+'/'+dir]) + '*'
         dirLinks = dirLinks[:-1]    # remove last *
         currentDirArray.append(dirLinks)
         allDirArray[currentDirId]=currentDirArray   # store currentDirArray on the correspondent position of allDirArray
@@ -93,41 +93,40 @@ def generateDirArray(dirToScan):
 
 
 def generateHTML(dirData,appName,appVer,genDate,genTime,title,appLink,numFiles,numDirs,grandTotalSize,linkFiles):
-	templateFile = open('template.html', 'r')
-	outputFile = open(title+'.html', 'w')
-	for line in templateFile:
-		modifiedLine = line
-		modifiedLine = modifiedLine.replace('[DIR DATA]', dirData)
-		modifiedLine = modifiedLine.replace('[APP NAME]', appName)
-		modifiedLine = modifiedLine.replace('[APP VER]', appVer)
-		modifiedLine = modifiedLine.replace('[GEN DATE]', genDate)
-		modifiedLine = modifiedLine.replace('[GEN TIME]', genTime)
-		modifiedLine = modifiedLine.replace('[TITLE]', title)
-		modifiedLine = modifiedLine.replace('[APP LINK]', appLink)
-		modifiedLine = modifiedLine.replace('[NUM FILES]', str(numFiles))
-		modifiedLine = modifiedLine.replace('[NUM DIRS]', str(numDirs))
-		modifiedLine = modifiedLine.replace('[TOT SIZE]', str(grandTotalSize))
-		modifiedLine = modifiedLine.replace('[LINK FILES]', linkFiles)
-		outputFile.write(modifiedLine)
-	templateFile.close()
-	outputFile.close()
-	
+    templateFile = open('template.html', 'r')
+    outputFile = open(title+'.html', 'w')
+    for line in templateFile:
+        modifiedLine = line
+        modifiedLine = modifiedLine.replace('[DIR DATA]', dirData)
+        modifiedLine = modifiedLine.replace('[APP NAME]', appName)
+        modifiedLine = modifiedLine.replace('[APP VER]', appVer)
+        modifiedLine = modifiedLine.replace('[GEN DATE]', genDate)
+        modifiedLine = modifiedLine.replace('[GEN TIME]', genTime)
+        modifiedLine = modifiedLine.replace('[TITLE]', title)
+        modifiedLine = modifiedLine.replace('[APP LINK]', appLink)
+        modifiedLine = modifiedLine.replace('[NUM FILES]', str(numFiles))
+        modifiedLine = modifiedLine.replace('[NUM DIRS]', str(numDirs))
+        modifiedLine = modifiedLine.replace('[TOT SIZE]', str(grandTotalSize))
+        modifiedLine = modifiedLine.replace('[LINK FILES]', linkFiles)
+        outputFile.write(modifiedLine)
+    templateFile.close()
+    outputFile.close()
+    
 
 
 
 
 # main program start point
-if len(sys.argv) < 3:	# check if required arguments are supplied
-	print "Missing arguments. This tool should be used as follows:"
-	print "	diogeneslist pathToIndex outputFileName"
+if len(sys.argv) < 3:    # check if required arguments are supplied
+    print ("Missing arguments. This tool should be used as follows:")
+    print ("    diogeneslist pathToIndex outputFileName")
 else:
-	pathToIndex = str(sys.argv[1])
-	title = str(sys.argv[2])
-	if os.path.exists(pathToIndex):	# check if the specified directory exists
-		generateDirArray(pathToIndex)
-		generateHTML(dirData,appName,appVer,genDate,genTime,title,appLink,numFiles,numDirs,grandTotalSize,linkFiles)
-	else:
-		print "The specified directory doesn't exist"
+    pathToIndex = str(sys.argv[1])
+    title = str(sys.argv[2])
+    if os.path.exists(pathToIndex):    # check if the specified directory exists
+        generateDirArray(pathToIndex)
+        generateHTML(dirData,appName,appVer,genDate,genTime,title,appLink,numFiles,numDirs,grandTotalSize,linkFiles)
+    else:
+        print("The specified directory doesn't exist")
 
-    
-        
+
